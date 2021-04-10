ddb3ede13abafb4a31138fbd53d24868499d744d
Paul Richardson <developer@photographerafoot.com>
2021-04-10 10:28:57 +1000

    CHG: implement table data source
    display each element of usedWords in a cell

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -29,7 +29,18 @@ class ViewController: UITableViewController {
 		usedWords.removeAll(keepingCapacity: true)
 		tableView.reloadData()
 	}
+	
+	// MARK: - Table view data source
 
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return usedWords.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+		cell.textLabel?.text = usedWords[indexPath.row]
+		return cell
+	}
 
 }
 
