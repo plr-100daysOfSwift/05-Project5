721736f0dc3d0ee346e42fe22d15941046f748da
Paul Richardson <developer@photographerafoot.com>
2021-04-10 10:09:46 +1000

    CHG: add two properties to hold an array of all words and an array of words entered by the user

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+	var allWords = [String]()
+	var usedWords = [String]()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
