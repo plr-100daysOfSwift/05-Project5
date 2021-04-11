fdcebf79a9dd98bc5a4736ffbe52f0823ecbc192
Paul Richardson <developer@photographerafoot.com>
2021-04-11 14:17:48 +1000

    CHG: isReal returns true if UITextChecker does not find a spelling error

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -75,7 +75,7 @@ class ViewController: UITableViewController {
 				return false
 			}
 		}
-		
+
 		return true
 	}
 
@@ -84,7 +84,10 @@ class ViewController: UITableViewController {
 	}
 
 	func isReal(word: String) -> Bool {
-		return true
+		let checker = UITextChecker()
+		let range = NSRange(location: 0, length: word.utf16.count)
+		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+		return misspelledRange.location == NSNotFound
 	}
 
 	// MARK: - Table view data source
