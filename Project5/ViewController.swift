c275ec2faf96a0413d23653854e6d2a34c5f3911
Paul Richardson <developer@photographerafoot.com>
2021-04-11 13:49:00 +1000

    CHG: if all three test pass, add the word to usedWords and insert a new row at the top of the table

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -53,6 +53,16 @@ class ViewController: UITableViewController {
 
 	func submit(_ answer: String) {
 		let lowerAnswer = answer.lowercased()
+
+		if isPossible(word: lowerAnswer) {
+			if isOriginal(word: lowerAnswer) {
+				if isReal(word: lowerAnswer) {
+					usedWords.insert(answer, at: 0)
+					let indexPath = IndexPath(row: 0, section: 0)
+					tableView.insertRows(at: [indexPath], with: .automatic)
+				}
+			}
+		}
 	}
 
 	func isPossible(word: String) -> Bool {
