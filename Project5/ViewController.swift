626485635bd9e3f80df69cf777f02ccad66da6ee
Paul Richardson <developer@photographerafoot.com>
2021-04-12 09:10:48 +1000

    CHG: add left navigation bar button
    Call startGame()
    startGame() requires @objc  attribute when called from UIBarButtonItem

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UITableViewController {
 
 		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
 
+		navigationItem.leftBarButtonItem = UIBarButtonItem(title: "New Game", style: .plain, target: self, action: #selector(startGame))
+
 		if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
 			if let startWords = try? String(contentsOf: startWordsURL) {
 				allWords = startWords.components(separatedBy: "\n")
@@ -30,7 +32,7 @@ class ViewController: UITableViewController {
 		startGame()
 	}
 
-	func startGame() {
+	@objc func startGame() {
 		title = allWords.randomElement()
 		usedWords.removeAll(keepingCapacity: true)
 		tableView.reloadData()
