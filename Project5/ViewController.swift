83b3ae40ff2bcd92aa6f9cf95938cb9564ee619f
Paul Richardson <developer@photographerafoot.com>
2021-05-03 12:32:41 +1000

    CHG: save used words to user defaults

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -12,13 +12,14 @@ class ViewController: UITableViewController {
 	var allWords = [String]()
 	var usedWords = [String]()
 	var referenceWord: String?
+	var defaults = UserDefaults.standard
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
 
-		navigationItem.leftBarButtonItem = UIBarButtonItem(title: "New Game", style: .plain, target: self, action: #selector(startGame))
+		navigationItem.leftBarButtonItem = UIBarButtonItem(title: "New Game", style: .plain, target: self, action: #selector(newGame))
 
 		if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
 			if let startWords = try? String(contentsOf: startWordsURL) {
@@ -30,15 +31,24 @@ class ViewController: UITableViewController {
 			allWords = ["silkworm"]
 		}
 
-		referenceWord = allWords.randomElement()!
+		if let usedWords = defaults.object(forKey: "UsedWords") as? [String] {
+			referenceWord = defaults.string(forKey: "Word")
+			title = referenceWord
+			self.usedWords = usedWords
+		} else {
+			newGame()
+		}
 
-		startGame()
 	}
 
-	@objc func startGame() {
+	@objc func newGame() {
+		referenceWord = allWords.randomElement()!
 		title = referenceWord
 		usedWords.removeAll(keepingCapacity: true)
 		tableView.reloadData()
+
+		defaults.set(referenceWord, forKey: "Word")
+		defaults.set(usedWords, forKey: "UsedWords")
 	}
 
 	@objc func promptForAnswer() {
@@ -63,6 +73,7 @@ class ViewController: UITableViewController {
 			if isOriginal(word: lowerAnswer) {
 				if isReal(word: lowerAnswer) {
 					usedWords.insert(lowerAnswer, at: 0)
+					defaults.set(usedWords, forKey: "UsedWords")
 					let indexPath = IndexPath(row: 0, section: 0)
 					tableView.insertRows(at: [indexPath], with: .automatic)
 					return
